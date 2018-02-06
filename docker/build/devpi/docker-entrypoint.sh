4fc465e579e92f1f47a51e4e1a7a96021b9acffd
John Eskew <jeskew@edx.org>
2018-02-05 20:55:53 -0500

    fixup! Add devpi Docker container building and a devpi Ansible role.

--- docker/build/devpi/docker-entrypoint.sh
+++ docker/build/devpi/docker-entrypoint.sh
@@ -12,11 +12,17 @@ function defaults {
 
 function initialize_devpi {
     echo "[RUN]: Initializing devpi-server..."
-    devpi-server --restrict-modify root --start --host 127.0.0.1 --port 3141
+    if [ ! -d  $DEVPI_SERVERDIR ]; then
+        devpi-server --restrict-modify root --init --start --host 127.0.0.1 --port 3141
+    else
+        devpi-server --restrict-modify root --start --host 127.0.0.1 --port 3141
+    fi
     devpi-server --status
     devpi use http://localhost:3141
     devpi login root --password=''
+    DEVPI_PASSWORD=`date +%s | sha256sum | base64 | head -c 32`
     devpi user -m root password="${DEVPI_PASSWORD}"
+    echo "[RUN]: devpi-server password set to '${DEVPI_PASSWORD}'" > $DEVPI_SERVERDIR/.serverpassword
     devpi index -y -c public pypi_whitelist='*'
     devpi-server --stop
     devpi-server --status
